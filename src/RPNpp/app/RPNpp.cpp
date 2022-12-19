12812bd5c31113972541c619f50f8cad6a899397
Jonathan Sönnerup <nooxet@gmail.com>
2022-12-19 01:23:05 +0100

    Wrote simple test program for events

--- src/RPNpp/app/RPNpp.cpp
+++ src/RPNpp/app/RPNpp.cpp
@@ -1,11 +1,71 @@
-import RPNpp.Stack;
+import RPNpp.stack;
+import RPNpp.utilities;
+
+#include <any>
 #include <iostream>
 
 using namespace RPNpp;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+/**
+ * Test classes for observing events
+ */
+namespace
+{
+	class StackErrorObserver : public Observer
+	{
+	public:
+		StackErrorObserver(string name) : Observer(name) {}
+
+
+		void onEvent(const std::any &data) override
+		{
+			try
+			{
+				const auto &d = std::any_cast<StackErrorData>(data);
+				cout << "Stack error event: " << StackErrorData::Message(d.error()) << endl;
+			}
+			catch (const std::bad_any_cast &)
+			{
+				cout << "Failed conversion" << endl;
+			}
+		}
+	};
+
+
+	class StackChangedObserver : public Observer
+	{
+	public:
+		StackChangedObserver(string name) : Observer(name) {}
+
+
+		void onEvent(const std::any &data) override
+		{
+			cout << "Stack changed" << endl;
+		}
+	};
+}
 
 
 int main()
 {
-	Stack::Instance().push(12.3);
-	std::cout << Stack::Instance().pop() << std::endl;
+	auto &stack = Stack::Instance();
+
+	// attack observers
+	stack.stackEvent.attach(Stack::stackError(), std::make_unique<StackErrorObserver>("StackErrorObserver"));
+	stack.stackEvent.attach(Stack::stackChanged(), std::make_unique<StackChangedObserver>("StackChangedObserver"));
+
+	try
+	{
+		stack.push(13.37);
+		cout << stack.pop() << endl;
+		stack.pop();
+	}
+	catch (Exception &e)
+	{
+		cout << e.what() << endl;
+	}
 }
